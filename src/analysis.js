41fb861609de58d64ad9fb9f40546ada15763d84
usser.email <user@MacBook-Air-de-user.local>
2022-03-02 14:48:10 +0100

    initial commit

--- src/analysis.js
+++ src/analysis.js
@@ -1,4 +1,4 @@
-const { getTrips } = require('api');
+const { getTrips, getDriver, getVehicle } = require("api");
 
 /**
  * This function should return the trip data analysis
@@ -7,7 +7,161 @@ const { getTrips } = require('api');
  * @returns {any} Trip data analysis
  */
 async function analysis() {
-  // Your code goes here
+  // variables
+  let noOfCashTrips = 0;
+  let noOfNonCashTrips = 0;
+  let billedTotal = 0;
+  let cashBilledTotal = 0;
+  let nonCashBilledTotal = 0;
+  let noOfDriversWithMoreThanOneVehicle = 0;
+  let totalAmountEarned = 0;
+
+  const arrOfMostTripDriver = [];
+  let driverIds = [];
+  let highestEarningDriverObj = {};
+  let getTrip;
+
+  // calling the getTrip api
+
+  getTrip = await getTrips();
+
+  getTrip.forEach((curr) => {
+    if (curr.isCash == true) {
+      noOfCashTrips++;
+      let cash1 = Number(curr.billedAmount.toString().split(",").join(""));
+      cashBilledTotal += cash1;
+    } else {
+      noOfNonCashTrips++;
+      let cash2 = Number(curr.billedAmount.toString().split(",").join(""));
+      nonCashBilledTotal += cash2;
+    }
+    let count = Number(curr.billedAmount.toString().split(",").join(""));
+    billedTotal += count;
+
+    driverIds.push(curr.driverID);
+
+    highestEarningDriverObj[curr.driverID] =
+      highestEarningDriverObj[curr.driverID] +
+        Number(curr.billedAmount.toString().split(",").join("")) ||
+      Number(curr.billedAmount.toString().split(",").join(""));
+  });
+
+  // checking for most trips by a driver by using the ids as key and the number of their trips as value
+  const driverObj = {};
+
+  driverIds.forEach((x) => {
+    driverObj[x] = driverObj[x] + 1 || 1;
+  });
+
+  //picking the highest value, to equal the most trips
+  let resultNum = 0;
+  let resultKey = "";
+  for (const [key, value] of Object.entries(driverObj)) {
+    if (value > resultNum) {
+      resultNum = value;
+      resultKey = key;
+    }
+  }
+
+  //picking the highest value, to equal the most earn
+  let noOfTripsForHigestEarnDriver = 0;
+  let idOfHighestEarnDriver = "";
+  for (const [keys, values] of Object.entries(highestEarningDriverObj)) {
+    if (values > totalAmountEarned) {
+      totalAmountEarned = values;
+      idOfHighestEarnDriver = keys;
+    }
+  }
+
+  //creating unique ids to avoid duplicate
+  const uniqueIds = [...new Set(driverIds)];
+
+  let mostTripsByDriverObj = [];
+  const highestEarnD = [];
+
+  // calling the driver api using the unique ids in a loop
+
+  let driverInfo = uniqueIds.map((each) => getDriver(each));
+  let allDrivers = await Promise.allSettled(driverInfo);
+
+  let withIdObj = {};
+
+  uniqueIds.forEach((curr, index) => (withIdObj[curr] = allDrivers[index]));
+
+  try {
+    for (const each in withIdObj) {
+      if (withIdObj[each].value.vehicleID.length > 1) {
+        noOfDriversWithMoreThanOneVehicle++;
+      }
+      if (each == resultKey) {
+        mostTripsByDriverObj.push(withIdObj[each].value);
+      }
+
+      if (each == idOfHighestEarnDriver) {
+        highestEarnD.push(withIdObj[each].value);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  // pushing all the trips of most trips driver in an array
+  const arrOfTotalAmountMost = [];
+  getTrip.forEach((x) => {
+    if (x.driverID == resultKey) {
+      arrOfTotalAmountMost.push(x);
+    }
+
+    if (x.driverID == idOfHighestEarnDriver) {
+      noOfTripsForHigestEarnDriver++;
+    }
+  });
+
+  // creating the most trips obj
+  let mostTripsByDriver;
+  mostTripsByDriverObj.forEach((each) => {
+    return (mostTripsByDriver = {
+      mostTripsByDriver: {
+        name: each.name,
+        email: each.email,
+        phone: each.phone,
+        noOfTrips: resultNum,
+        totalAmountEarned: arrOfTotalAmountMost.reduce((acc, curr) => {
+          return acc + Number(curr.billedAmount.toString().split(",").join(""));
+        }, 0),
+      },
+    });
+  });
+
+  // creating the higest earn driver obj
+  let highestEarningDriver;
+  highestEarnD.forEach((each) => {
+    return (highestEarningDriver = {
+      highestEarningDriver: {
+        name: each.name,
+        email: each.email,
+        phone: each.phone,
+        noOfTrips: noOfTripsForHigestEarnDriver,
+        totalAmountEarned: Number(totalAmountEarned.toFixed(2)),
+      },
+    });
+  });
+
+  // constructing the output
+  const output = {
+    noOfCashTrips: noOfCashTrips,
+    noOfNonCashTrips: noOfNonCashTrips,
+    billedTotal: Number(billedTotal.toFixed(2)),
+    cashBilledTotal: Number(cashBilledTotal.toFixed(2)),
+    nonCashBilledTotal: Number(nonCashBilledTotal.toFixed(2)),
+    noOfDriversWithMoreThanOneVehicle: noOfDriversWithMoreThanOneVehicle,
+    ...mostTripsByDriver,
+    ...highestEarningDriver,
+  };
+
+  // console.log(output);
+  return output;
 }
+analysis();
 
 module.exports = analysis;
--- src/report.js
+++ src/report.js
@@ -1,5 +1,5 @@
-const { getTrips } = require('api');
-
+const { getTrips, getDriver, getVehicle } = require("api");
+// import {withIdObj} from './analysis.js'
 /**
  * This function should return the data for drivers in the specified format
  *
@@ -7,8 +7,155 @@ const { getTrips } = require('api');
  *
  * @returns {any} Driver report data
  */
+
+//
+
 async function driverReport() {
-  // Your code goes here
+  const driverIds = [];
+
+  // calling the getTrip api
+  getTrip = await getTrips();
+
+  //pushing the driverIds to array
+  getTrip.forEach((curr) => {
+    driverIds.push(curr.driverID);
+  });
+
+  //creating unique ids
+  const uniqueIds = [...new Set(driverIds)];
+
+  // using two point method to group drivers and their trips
+  const subGroups = [];
+  for (const i of uniqueIds) {
+    let temporaryArr = [];
+    for (const j of getTrip) {
+      if (i == j.driverID) {
+        temporaryArr.push(j);
+      }
+    }
+    subGroups.push(temporaryArr);
+    temporaryArr = [];
+  }
+
+  // using unique ids to call the driver info
+  let driverInfo = uniqueIds.map((each) => getDriver(each));
+  let allDrivers = await Promise.allSettled(driverInfo);
+
+  // process of adding drivers id to the info gotten
+  let withIdObj = {};
+
+  uniqueIds.forEach((curr, index) => (withIdObj[curr] = allDrivers[index]));
+
+  const vehicleIds = [];
+
+  // using the info gottent to get vehicle info
+  try {
+    for (const each in withIdObj) {
+      vehicleIds.push(withIdObj[each].value.vehicleID);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  // turning the vehidle ids arrays to 1D array
+  const flattenVehicleIds = vehicleIds.flat();
+
+  //using the flattenVehicleIds to call the vehicle function
+  let vehicleInfo = flattenVehicleIds.map((each) => getVehicle(each));
+  let allVehicle = await Promise.allSettled(vehicleInfo);
+
+  // adding drivers id to the vehicle info
+  let withVehicleId = {};
+
+  flattenVehicleIds.forEach(
+    (curr, index) => (withVehicleId[curr] = allVehicle[index])
+  );
+
+  try {
+    for (const each in withVehicleId) {
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  // funtion for creation of trips info
+  function createTripsInfo(index) {
+    const driver = uniqueIds[index];
+    const trips = subGroups[index];
+    const driverTrips = [];
+    const tripsFact = trips.map((trip, index) => {
+      const ans = {
+        user: trip.user.name,
+        created: trip.created,
+        pickup: trip.pickup.address,
+        destination: trip.destination.address,
+        billed: +trip.billedAmount.toString().split(",").join(""),
+        isCash: trip.isCash,
+      };
+      driverTrips.push(ans);
+    });
+    return driverTrips;
+  }
+
+  // creation of the final result object
+  let totalResult = [];
+  let count = 0;
+  try {
+    totalResult = uniqueIds.forEach((id, index) => {
+      return totalResult.push({
+        fullName: withIdObj[id].value.name,
+        phone: withIdObj[id].value.phone,
+        id: id,
+        // vehicles:
+        noOfTrips: subGroups[index].length,
+        noOfCashTrips: subGroups[index]
+          .filter(({ isCash }) => isCash === true)
+          .reduce((acc, curr, index, arr) => arr.length, 0),
+        noOfNonCashTrips: subGroups[index]
+          .filter(({ isCash }) => isCash === false)
+          .reduce((acc, curr, index, arr) => arr.length, 0),
+        trips: createTripsInfo(index),
+        totalAmountEarned: subGroups[index].reduce((acc, curr, index, arr) => {
+          return (
+            acc +
+            Number(
+              Number(curr.billedAmount.toString().split(",").join("")).toFixed(
+                2
+              )
+            )
+          );
+        }, 0),
+        totalCashAmount: subGroups[index]
+          .filter(({ isCash }) => isCash === true)
+          .reduce(
+            (acc, curr, index, arr) =>
+              acc +
+              Number(
+                Number(
+                  curr.billedAmount.toString().split(",").join("")
+                ).toFixed(2)
+              ),
+            0
+          ),
+        totalNonCashAmount: subGroups[index]
+          .filter(({ isCash }) => isCash === false)
+          .reduce(
+            (acc, curr, index, arr) =>
+              acc +
+              Number(
+                Number(
+                  curr.billedAmount.toString().split(",").join("")
+                ).toFixed(2)
+              ),
+            0
+          ),
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  return totalResult;
 }
+driverReport();
 
 module.exports = driverReport;
